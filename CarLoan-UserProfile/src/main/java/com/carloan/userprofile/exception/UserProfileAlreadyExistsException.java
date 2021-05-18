cff5f579e4891fb6483fcd81443d911de9465ebd
keerthanacse13@gmail.com <keerthanacse13@gmail.com>
2021-05-18 22:08:03 +0800

    User already exists exception added

---
+++ CarLoan-UserProfile/src/main/java/com/carloan/userprofile/exception/UserProfileAlreadyExistsException.java
@@ -0,0 +1,8 @@
+package com.carloan.userprofile.exception;
+
+public class UserProfileAlreadyExistsException extends Exception {
+
+    public UserProfileAlreadyExistsException(String message) {
+        super(message);
+    }
+}
