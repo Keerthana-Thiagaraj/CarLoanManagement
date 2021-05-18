10fc62767dc1dc824c56296874ae2cc991396ba7
keerthanacse13@gmail.com <keerthanacse13@gmail.com>
2021-05-18 21:58:08 +0800

    Added userprofile service layer

--- CarLoan-UserProfile/src/main/java/com/carloan/userprofile/service/UserProfileService.java
+++ CarLoan-UserProfile/src/main/java/com/carloan/userprofile/service/UserProfileService.java
@@ -1,4 +1,13 @@
 package com.carloan.userprofile.service;
 
+import com.carloan.userprofile.exception.UserProfileAlreadyExistsException;
+import com.carloan.userprofile.model.UserProfile;
+
+import java.util.List;
+
 public interface UserProfileService {
+
+    List<UserProfile> getAllUsers();
+
+    UserProfile registerUser(UserProfile userProfile) throws UserProfileAlreadyExistsException;
 }
--- CarLoan-UserProfile/src/main/java/com/carloan/userprofile/service/UserProfileServiceImpl.java
+++ CarLoan-UserProfile/src/main/java/com/carloan/userprofile/service/UserProfileServiceImpl.java
@@ -1,4 +1,39 @@
 package com.carloan.userprofile.service;
 
-public class UserProfileServiceImpl {
+import com.carloan.userprofile.exception.UserProfileAlreadyExistsException;
+import com.carloan.userprofile.model.UserProfile;
+import com.carloan.userprofile.repository.UserProfileRepository;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@Slf4j
+public class UserProfileServiceImpl implements UserProfileService {
+
+    @Autowired
+    private UserProfileRepository userProfileRepository;
+
+    @Override
+    public List<UserProfile> getAllUsers() {
+        return userProfileRepository.findAll();
+    }
+
+    @Override
+    public UserProfile registerUser(UserProfile userProfile) throws UserProfileAlreadyExistsException {
+
+        try {
+            if (userProfileRepository.findByEmail(userProfile.getEmail()).size() > 0) {
+                throw new UserProfileAlreadyExistsException("Error");
+            } else if (userProfileRepository.save(userProfile) == null) {
+                throw new UserProfileAlreadyExistsException("Error");
+            } else {
+                return userProfile;
+            }
+        } catch (Exception e) {
+            throw new UserProfileAlreadyExistsException("Error");
+        }
+    }
 }
