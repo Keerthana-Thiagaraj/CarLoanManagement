9e12a646cb5bef395ca840b4ecc6eb186eb60438
keerthanacse13@gmail.com <keerthanacse13@gmail.com>
2021-05-19 14:22:54 +0800

    User not found exception added

---
+++ CarLoan-UserProfile/src/main/java/com/carloan/userprofile/exception/UserProfileNotFoundException.java
@@ -0,0 +1,8 @@
+package com.carloan.userprofile.exception;
+
+public class UserProfileNotFoundException extends Exception {
+
+    public UserProfileNotFoundException(String message) {
+        super(message);
+    }
+}
