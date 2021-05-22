32c55d6fe1e62a1cba5fc1e09f35dbf446d00d7d
keerthanacse13@gmail.com <keerthanacse13@gmail.com>
2021-05-22 13:02:24 +0800

    Enabling eureka clients

--- CarLoan-LoanProcessing/src/main/java/com/carloan/loanprocessing/LoanProcessingApplication.java
+++ CarLoan-LoanProcessing/src/main/java/com/carloan/loanprocessing/LoanProcessingApplication.java
@@ -2,12 +2,25 @@ package com.carloan.loanprocessing;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
+
 
 @SpringBootApplication
+@EnableEurekaClient
+@EnableFeignClients
 public class LoanProcessingApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(LoanProcessingApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(LoanProcessingApplication.class, args);
+    }
 
+    @Bean
+    @LoadBalanced
+    public RestTemplate getRestTemplate() {
+        return new RestTemplate();
+    }
 }
--- CarLoan-UserProfile/src/main/java/com/carloan/userprofile/CarloanUserprofileApplication.java
+++ CarLoan-UserProfile/src/main/java/com/carloan/userprofile/CarloanUserprofileApplication.java
@@ -2,8 +2,10 @@ package com.carloan.userprofile;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class CarloanUserprofileApplication {
 
 	public static void main(String[] args) {
