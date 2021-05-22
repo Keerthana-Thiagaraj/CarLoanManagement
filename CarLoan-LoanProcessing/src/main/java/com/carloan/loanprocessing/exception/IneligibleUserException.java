bc482f533e5b2166899fe1304dc1d57c259daf7d
keerthanacse13@gmail.com <keerthanacse13@gmail.com>
2021-05-22 13:00:37 +0800

    Custom exceptions added

---
+++ CarLoan-LoanProcessing/src/main/java/com/carloan/loanprocessing/exception/IneligibleUserException.java
@@ -0,0 +1,8 @@
+package com.carloan.loanprocessing.exception;
+
+public class IneligibleUserException extends Exception {
+
+    public IneligibleUserException(String message) {
+        super(message);
+    }
+}
---
+++ CarLoan-LoanProcessing/src/main/java/com/carloan/loanprocessing/exception/UserProfileNotFoundException.java
@@ -0,0 +1,8 @@
+package com.carloan.loanprocessing.exception;
+
+public class UserProfileNotFoundException extends Exception {
+
+    public UserProfileNotFoundException(String message) {
+        super(message);
+    }
+}
