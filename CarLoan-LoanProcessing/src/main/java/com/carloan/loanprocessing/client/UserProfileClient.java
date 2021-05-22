16835af08fccf514bd06eb2d00826e9476f5521f
keerthanacse13@gmail.com <keerthanacse13@gmail.com>
2021-05-22 15:14:44 +0800

    unused imports removed

--- CarLoan-LoanProcessing/src/main/java/com/carloan/loanprocessing/client/UserProfileClient.java
+++ CarLoan-LoanProcessing/src/main/java/com/carloan/loanprocessing/client/UserProfileClient.java
@@ -4,7 +4,6 @@ package com.carloan.loanprocessing.client;
 import com.carloan.loanprocessing.model.UserProfile;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.Mapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
