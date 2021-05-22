8866e6057df1ee311fa693dbc15b46733e97fbd9
keerthanacse13@gmail.com <keerthanacse13@gmail.com>
2021-05-22 12:57:36 +0800

    Communication to user service- Feign client added

---
+++ CarLoan-LoanProcessing/src/main/java/com/carloan/loanprocessing/client/UserProfileClient.java
@@ -0,0 +1,21 @@
+package com.carloan.loanprocessing.client;
+
+
+import com.carloan.loanprocessing.model.UserProfile;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.Mapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.Optional;
+
+@FeignClient("UserProfile-Service")
+public interface UserProfileClient {
+
+    @GetMapping("/api/v1/user/{userId}")
+    Optional<UserProfile> findUserProfileById(@PathVariable("userId") int user_id);
+
+    @PostMapping("/api/v1/user/checkLoanEligibility/{userId}")
+    boolean getLoanEligibilityStatus(@PathVariable("userId") int user_id);
+}
